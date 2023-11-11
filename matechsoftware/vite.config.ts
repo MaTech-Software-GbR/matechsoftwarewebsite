306b585bd8fd166424490ba4bdd05096c1c66f91
Maximilian Gobbel <maxigobbel@googlemail.com>
2023-11-12 00:05:07 +0100

    fixed errors

--- matechsoftware/vite.config.ts
+++ matechsoftware/vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
-import ViteSassPlugin from 'vite-plugin-sass';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import ViteSassPlugin from 'vite-plugin-sass';
+// import viteTsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  // depending on your application, base can also be "/"
-  base: '',
-  plugins: [react(), viteTsconfigPaths(), ViteSassPlugin()],
-  server: {
-    // this ensures that the browser opens upon server start
-    open: true,
-    // this sets a default port to 3000
-    port: 3000,
-  },
-});
+// export default defineConfig({
+//   // depending on your application, base can also be "/"
+//   base: '',
+//   plugins: [react(), viteTsconfigPaths(), ViteSassPlugin()],
+//   server: {
+//     // this ensures that the browser opens upon server start
+//     open: true,
+//     // this sets a default port to 3000
+//     port: 3000,
+//   },
+// });
