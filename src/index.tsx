1e185c1c773db73c258a919826ec60e612c73e21
Maximilian Gobbel <maxigobbel@googlemail.com>
2023-11-16 19:33:49 +0100

    lazy loading inside index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Impressum from './pages/Impressum';
-import Datenschutz from './pages/Datenschutz';
-import ErrorPage from './error-page';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import './styles/Base.scss';
-import './styles/App.scss';
-import App from './App';
+import React, { Suspense } from "react"
+import ReactDOM from "react-dom/client"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import ErrorPage from "./error-page"
+import AOS from "aos"
+import "aos/dist/aos.css"
+import "./styles/Base.scss"
+import "./styles/App.scss"
 
-AOS.init();
+AOS.init()
+
+const App = React.lazy(async () => await import("./App"))
+const Impressum = React.lazy(async () => await import("./pages/Impressum"))
+const Datenschutz = React.lazy(async () => await import("./pages/Datenschutz"))
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <App />,
+    path: "/",
+    element: (
+      <Suspense fallback={<div>MaTech lädt...</div>}>
+        <App />
+      </Suspense>
+    ),
     errorElement: <ErrorPage />
   },
   {
-    path: '/impressum',
-    element: <Impressum />,
+    path: "/impressum",
+    element: (
+      <Suspense fallback={<div>MaTech lädt...</div>}>
+        <Impressum />
+      </Suspense>
+    ),
     errorElement: <ErrorPage />
   },
   {
-    path: '/datenschutz',
-    element: <Datenschutz />,
+    path: "/datenschutz",
+    element: (
+      <Suspense fallback={<div>MaTech lädt...</div>}>
+        <Datenschutz />
+      </Suspense>
+    ),
     errorElement: <ErrorPage />
   }
-]);
+])
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-);
+)
